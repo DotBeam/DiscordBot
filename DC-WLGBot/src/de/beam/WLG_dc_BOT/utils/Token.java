ad09feba0efca163c8ae4cf06066cdc1af81bcb0
DotBeam <69441866+DotBeam@users.noreply.github.com>
2021-08-01 19:54:56 +0200

    Delete Token.java

--- DC-WLGBot/src/de/beam/WLG_dc_BOT/utils/Token.java
+++
@@ -1,9 +0,0 @@
-package de.beam.WLG_dc_BOT.utils;
-
-public class Token {
-	
-	public static final String TOKEN_STRING = "ODY4OTMwNTEyNzA4ODQ1NTg5.YP20mg.cqSErtFTQYRG_VICEoOb1DkkYd4";
-	public static final String TWICHTOKEN_STRING = "tof5qiyt0lj6xs6rr54slxa1pesnck";
-	public static final String TwitchClientid = "s7a34mdr4olhezbefao2j4urngspvz";
-	public static final String TwitchClientsecret = "deu52n1ut9c9rmghkq62wzpaubvl5v";
-}
